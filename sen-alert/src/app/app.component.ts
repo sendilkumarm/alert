0fc018b83389a9001487dc0b7dd38f123f57fdc2
sendilkumar <semu4099@gmail.com>
2019-01-05 20:40:14 +0530

    Sample first few alerts in html

--- sen-alert/src/app/app.component.ts
+++ sen-alert/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  messages: string[] = [];
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    this.messages.push('Error on Date of Birth');
+    this.messages.push('Error on Date of Birth');
+    this.messages.push('Error on Date of Birth');
+    this.messages.push('Error on Date of Birth');
+  }
 }
